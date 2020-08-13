7cf0de4f2c4c43583e76943d6eea68b0455c9590
Nk04 <n.kazuma1998@gmail.com>
2020-08-13 13:09:40 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,11 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   KANA_REGEX = /\A[ァ-ン]+\z/.freeze
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
 
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
   validates :nick_name, presence: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }, confirmation: true
-  validates :password_confirmation, presence: true
   validates :family_name, presence: true, format: { with: NAME_REGEX }
   validates :first_name, presence: true, format: { with: NAME_REGEX }
   validates :family_name_kana, presence: true, format: { with: KANA_REGEX }
