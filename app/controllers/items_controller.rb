5ebe968b4f431f3ec044d7c24e018e0b8eec52bf
Nk04 <n.kazuma1998@gmail.com>
2020-08-18 19:20:41 +0900

    before_actionの設定修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, only: [:new]
   def index
     @item = Item.order('created_at DESC')
   end
@@ -27,9 +27,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :name, :comment, :image, :category_id, :status_id, :delivery_fee_id, :shipping_origin_id, :shipping_date_id, :price
     ).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to user_session_path unless user_signed_in?
   end
 end
