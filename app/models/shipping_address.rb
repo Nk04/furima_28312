6945a40a8603603001810c49f70aaf016c225f2c
Nk04 <n.kazuma1998@gmail.com>
2020-08-20 16:55:20 +0900

    activehashの導入

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,7 @@
 class ShippingAddress < ApplicationRecord
   # association
   belongs_to :item
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
