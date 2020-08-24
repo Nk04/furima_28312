a06623faff27bb979d541fb2395401d4b8a0ea7b
Nk04 <n.kazuma1998@gmail.com>
2020-08-24 15:42:49 +0900

    item_purchaseのassociation設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one :shipping_address
-
+  has_one :item_purchase
 
   # validation
   with_options presence: true do
