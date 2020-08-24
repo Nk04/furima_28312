cc2951c7012ce15aaa66e667294b368453068528
Nk04 <n.kazuma1998@gmail.com>
2020-08-24 12:22:38 +0900

    pay.jpによる決済機能の導入

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,5 +1,5 @@
 class ShippingAddressesController < ApplicationController
-  before_action :set_item, only: [:index]
+  before_action :set_item
 
   def index
     @shipping_address = ShippingAddress.new
@@ -8,6 +8,7 @@ class ShippingAddressesController < ApplicationController
   def create
     @shipping_address = ShippingAddress.new(shipping_address_params)
     @shipping_address.save!
+    pay_item
     redirect_to root_path
   rescue StandardError
     redirect_to item_shipping_addresses_path
@@ -17,8 +18,21 @@ class ShippingAddressesController < ApplicationController
 
   def shipping_address_params
     params.require(:shipping_address).permit(
-      :postal_code, :prefectures_id, :city, :house_number, :building_name, :phone_number
+      :postal_code, :prefectures_id, :city, :house_number, :building_name, :phone_number, :token
     ).merge(item_id: params[:item_id])
+  end
+
+  def card_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_74a4cb08b703b0636e8d13af"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: card_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def set_item
